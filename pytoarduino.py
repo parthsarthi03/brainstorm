ffcb9bf711b4051aea0a1010dc90319d2971f921
Kenan Hasanaliyev <kenan@DN51vcc4.SUNet>
2022-02-20 08:18:30 -0800

    arduino code

---
+++ pytoarduino.py
@@ -0,0 +1,14 @@
+﻿# Importing Libraries
+import serial
+import time
+arduino = serial.Serial(port='COM4', baudrate=115200, timeout=.1)
+def write_read(x):
+    arduino.write(bytes(x, 'utf-8'))
+    time.sleep(0.05)
+    data = arduino.readline()
+    return data
+while True:
+    if(main.getAction() == “Go”):
+            write_read(“1”)
+    else:
+         write_read(“0”)
